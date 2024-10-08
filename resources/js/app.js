242b663b01ba2cf58966880c0f52d95826bd000c
alvisjohnmark <johnmarkalvis02@gmail.com>
2024-10-09 00:17:26 +0800

    icon lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,6 @@ import { createApp, markRaw } from "vue/dist/vue.esm-bundler.js";
 import { createRouter, createWebHistory, useRoute } from "vue-router";
 import { createPinia } from "pinia";
 import { routes } from "./router";
-import PrimeVue from 'primevue/config';
-import Aura from '@/presets/aura'; 
 
 const app = createApp({});
 const pinia = createPinia();
@@ -17,11 +15,6 @@ const router = createRouter({
 pinia.use(({ store }) => {
     store.router = markRaw(router);
     store.route = markRaw(useRoute());
-});
-
-app.use(PrimeVue, {
-    unstyled: true,
-    pt: Aura                           
 });
 
 app.use(pinia);
