d5a0a0314d16b174354cd91e1baabf8f78e17e0b
alvisjohnmark <johnmarkalvis02@gmail.com>
2024-11-01 23:15:19 +0800

    fetch datas for visitors

--- app/Http/Controllers/appController.php
+++ app/Http/Controllers/appController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Service;
 
 class appController extends Controller
 {
@@ -10,4 +11,18 @@ class appController extends Controller
         return view("app");
     }
 
+    public function getProducts()
+    {
+        $products = Product::with(['pictures:id,product_id,file_path'])->get();
+
+        return response()->json($products);
+    }
+
+    public function getServices()
+    {
+        $services = Service::all();
+        return response()->json($services);
+    }
+
 }
+    
