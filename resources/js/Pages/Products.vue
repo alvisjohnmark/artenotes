68280a58328599165c8ad66c85b2e6657b808c85
alvisjohnmark <johnmarkalvis02@gmail.com>
2024-11-24 21:16:33 +0800

    gtag

--- resources/js/Pages/Products.vue
+++ resources/js/Pages/Products.vue
@@ -105,64 +105,6 @@ onMounted(() => {
                     </div>
                 </div>
             </div>
-            <div
-                v-if="render.showAddressModal"
-                class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-            >
-                <div class="bg-white p-8 rounded-lg shadow-lg w-96">
-                    <h2 class="text-2xl font-semibold mb-4">
-                        Shipping Address
-                    </h2>
-
-                    <form @submit.prevent="render.saveAddress()">
-                        <div class="mb-4">
-                            <label class="block font-medium">Street</label>
-                            <input
-                                type="text"
-                                v-model="render.address.street"
-                                class="w-full p-2 border rounded-md"
-                                required
-                            />
-                        </div>
-                        <div class="mb-4">
-                            <label class="block font-medium"
-                                >City/Municipality</label
-                            >
-                            <input
-                                type="text"
-                                v-model="render.address.city_municipality"
-                                class="w-full p-2 border rounded-md"
-                                required
-                            />
-                        </div>
-                        <div class="mb-4">
-                            <label class="block font-medium">Province</label>
-                            <input
-                                type="text"
-                                v-model="render.address.province"
-                                class="w-full p-2 border rounded-md"
-                                required
-                            />
-                        </div>
-                        <div class="mb-4">
-                            <label class="block font-medium">Zipcode</label>
-                            <input
-                                type="text"
-                                v-model="render.address.zipcode"
-                                class="w-full p-2 border rounded-md"
-                                required
-                            />
-                        </div>
-
-                        <button
-                            type="submit"
-                            class="mt-4 w-full bg-lptxcolor text-white py-2 rounded-lg font-semibold"
-                        >
-                            Submit Address
-                        </button>
-                    </form>
-                </div>
-            </div>
         </main>
     </section>
     <Footer />
